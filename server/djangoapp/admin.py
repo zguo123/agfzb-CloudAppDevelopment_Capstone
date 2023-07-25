2e058fe432899aeb4b44aea314a3c87904ea80bf
Zhaoyu <zhaoyu.guo@mail.thecustomercompany.ca>
2023-07-25 17:44:39 -0400

    Updated admin and database

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,28 @@
 from django.contrib import admin
-# from .models import related models
+
+from djangoapp.models import CarMake, CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 2
+
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'dealer_id', 'car_type', 'year']
 
 # CarMakeAdmin class with CarModelInline
 
+
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    inlines = [CarModelInline]
+
+
 # Register models here
+admin.site.register(CarModel, CarModelAdmin)
+admin.site.register(CarMake, CarMakeAdmin)
