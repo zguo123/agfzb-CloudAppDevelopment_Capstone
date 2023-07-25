debd37a49b2d43e095388233408309e6e39abb76
Zhaoyu <zhaoyu.guo@mail.thecustomercompany.ca>
2023-07-25 10:59:49 -0400

    Module 2 - Finished Signup and auth

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,11 +14,15 @@ urlpatterns = [
 
     # path for contact us view
     path(route='contact', view=views.contact, name='contact'),
+    
     # path for registration
+    path(route='signup', view=views.registration_request, name='signup'),
 
     # path for login
+    path(route='login', view=views.login_request, name='login'),
 
     # path for logout
+    path(route='logout', view=views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
